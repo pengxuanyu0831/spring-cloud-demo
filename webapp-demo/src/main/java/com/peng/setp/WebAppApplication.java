e7c4f8b7fabe0adf4236071f29cc449510455833
pengxy <pengxuanyus@gmail.com>
2020-09-21 21:44:54 +0800

    get√

--- webapp-demo/src/main/java/com/peng/setp/WebAppApplication.java
+++ webapp-demo/src/main/java/com/peng/setp/WebAppApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class WebAppApplication {
+
     public static void main(String[] args){
         SpringApplication.run(WebAppApplication.class,args);
-
     }
 }
