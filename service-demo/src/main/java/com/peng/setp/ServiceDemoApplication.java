58b05130bf3f90e1de77a5eb44d3c3d96f9fda32
pengxy <pengxuanyus@gmail.com>
2020-09-21 22:18:34 +0800

    maven 不太好用。。。

--- service-demo/src/main/java/com/peng/setp/ServiceDemoApplication.java
+++ service-demo/src/main/java/com/peng/setp/ServiceDemoApplication.java
@@ -3,6 +3,7 @@ package com.peng.setp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 public class ServiceDemoApplication {
+
+    // 开启ribbon
+    @LoadBalanced
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
