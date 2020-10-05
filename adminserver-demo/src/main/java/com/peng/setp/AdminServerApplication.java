ed167da01eb914e53daa3aaf160eda1daa89a850
pengxy <pengxuanyus@gmail.com>
2020-10-05 10:27:15 +0800

    admin server管理中心模块

---
+++ adminserver-demo/src/main/java/com/peng/setp/AdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.peng.setp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class AdminServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AdminServerApplication.class,args);
+    }
+}
