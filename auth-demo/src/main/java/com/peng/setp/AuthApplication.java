d7288967c209bb09fb1d5372a67fec39ad4e355a
pengxy <pengxuanyus@gmail.com>
2020-10-07 17:51:43 +0800

    AuthApplication启动类

---
+++ auth-demo/src/main/java/com/peng/setp/AuthApplication.java
@@ -0,0 +1,11 @@
+package com.peng.setp;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class AuthApplication {
+}
